7d5d094e43ad978d9500d7c98eaf5d0bf0c77983
Zoltan Szeredi <zoltan.szeredi@3pillarglobal.com>
2014-10-18 16:53:41 +0300

    Update settings.py

--- photodrop/photodrop/settings.py
+++ photodrop/photodrop/settings.py
@@ -89,8 +89,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-AWS_ACCESS_KEY_ID = 'AKIAISTTUDEHYS7WMXPA' # os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = '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' # os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'troglodyte.photodrop'
 
 
